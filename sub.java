519805b3e74805c57e521056b632d1456f263fb1
Gokulnathan0605 <97106980+Gokulnathan0605@users.noreply.github.com>
2023-08-06 11:55:25 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class adding{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int sum =b-a;
+        System.out.println(sum);
+    }
+}
