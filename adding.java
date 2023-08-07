38584fea567165dd14903380aff0d20f4ef73001
Gokulnathan0605 <97106980+Gokulnathan0605@users.noreply.github.com>
2023-08-07 12:41:10 +0530

    change a to 20

--- adding.java
+++ adding.java
@@ -2,7 +2,7 @@ package Practice;
 
 public class adding {
     public static void main(String[] args) {
-        int a = 15;
+        int a = 20;
         int b = 20;
         int sum = a + b;
         System.out.println(sum);
